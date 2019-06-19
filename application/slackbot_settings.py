e65657c516ee079d2336148aa2a00a8aade3b55b
kab0718 <kab3nan9@gmail.com>
2019-06-19 10:52:00 +0900

    APIを隠した

--- application/slackbot_settings.py
+++ application/slackbot_settings.py
@@ -1,10 +1,6 @@
 import os
-from pathlib import  Path
-from dotenv import load_dotenv
-load_dotenv(verbose=True)
-load_dotenv(dotenv_path = Path('.') / '.env')
 
-API_TOKEN = os.getenv("API_KEY")
+API_TOKEN = os.environ['SLACK_API_KEY']
 
 default_reply = "すいません。よくわかりませんでした"
 
